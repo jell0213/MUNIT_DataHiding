4c3928d87309114eac74afa747814c3443b2da44
JasonLi <jjason41004tw@gmail.com>
2021-05-12 16:48:15 +0800

    Add files via upload

---
+++ 第二篇論文/算資料庫嵌入量.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 10:39:00 2021
+
+@author: li
+"""
+
+import math
+
+a=math.log(110250,2)
+print(a)
+print(a*64)
+a = 0.3
+b = 1 - a
+count = 0
+'''
+na = 0
+nb = 3
+num = 3
+count+=pow(a,na)*pow(b,nb)*num
+print(pow(a,na)*pow(b,nb)*num)
+
+
+na = 1
+nb = 2
+num = 3
+count+=pow(a,na)*pow(b,nb)*num
+count+=pow(a,na)*pow(b,nb)*num
+count+=pow(a,na)*pow(b,nb)*num
+print(pow(a,na)*pow(b,nb)*num)
+
+
+na = 2
+nb = 0
+num = 2
+count+=pow(a,na)*pow(b,nb)*num
+print(pow(a,na)*pow(b,nb)*num)
+
+
+na = 2
+nb = 1
+num = 3
+count+=pow(a,na)*pow(b,nb)*num
+count+=pow(a,na)*pow(b,nb)*num
+print(pow(a,na)*pow(b,nb)*num)
+print(count)
+'''
